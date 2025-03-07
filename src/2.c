c337b1ae14846e62e5e2693614f4ec896d22dd51
vladiknaildesigner937 <vladiknaildesigner937@gmail.com>
2025-03-07 00:48:59 +0000

    Add 2.c

---
+++ src/2.c
@@ -0,0 +1,22 @@
+// Math Homework Code
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  // Declare variables
+  int a = 0;
+  int b = 0;
+  int sum = 0;
+
+  // Ask user for input
+  printf("Enter two numbers: ");
+  scanf("%d %d", &a, &b);
+
+  // Calculate sum
+  sum = a + b;
+
+  // Print result
+  printf("The sum of the two numbers is: %d\n", sum);
+
+  return 0;
+}
