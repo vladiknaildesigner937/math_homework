b11b5582c4ce2e24a9ab2434df8706bab494d711
vladiknaildesigner937 <vladiknaildesigner937@gmail.com>
2025-03-08 20:10:57 +0000

    Add 3.c

---
+++ src/3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+    int result = num1 + num2;
+    printf("The sum of %d and %d is %d\n", num1, num2, result);
+    return 0;
+}
