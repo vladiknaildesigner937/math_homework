885cfca09520daae647191e23900b77f3459cc7d
vladiknaildesigner937 <vladiknaildesigner937@gmail.com>
2025-03-16 17:42:13 +0000

    Add 10.c

---
+++ src/10.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    int x = 0;
+    for (x=1; x<=20; x++) {
+        printf("Math homework done by the students.\n");
+    }
+    return 0;
+}
